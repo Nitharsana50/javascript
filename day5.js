0bc3110be8e9c5eff7226f92b43aab8c40353c3b
Nitharsana50 <24bct134@kgcas.com>
2025-07-29 10:49:08 +0530

    day5.js

---
+++ day5.js
@@ -0,0 +1,14 @@
+// let number = 786807141;
+// let pattern = /^[6-9]\d{9}5/;
+// if(pattern.test(number.toString())) {
+//     console.log("correct");
+// } else {
+//     console.log("Wrong pattern");
+// }
+// let text="Hai hello";
+// let match=/\b\w+\b/gi;
+// console.log(match ? text : 0)
+// console.log(text.length);
+// const text = "hello! how are you";
+// const words=text.match(/\b\w+\b/gi);
+// console.log(words.length);
